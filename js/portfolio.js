2fa7835a8968affa4d44d61820809f78d023087e
katieseo1 <seo.katie@gmail.com>
2017-03-24 15:21:11 -0700

    pic

---
+++ js/portfolio.js
@@ -0,0 +1,11 @@
+(function($) {
+  'use strict'
+  $('.page-scroll a').bind('click', function(event) {
+    var $anchor = $(this)
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo')
+    event.preventDefault()
+  })
+  
+})(jQuery)
