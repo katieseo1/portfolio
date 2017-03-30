1993ec282cfe4ccd90fa7a010ddc8dd5a3a95af3
katieseo1 <seo.katie@gmail.com>
2017-03-30 08:09:19 -0700

    edit

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,11 +1,11 @@
 (function($) {
-  'use strict'
-  $('.page-scroll a').bind('click', function(event) {
-    var $anchor = $(this)
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    }, 1250, 'easeInOutExpo')
-    event.preventDefault()
-  })
-  
+	'use strict'
+	$('.page-scroll a').bind('click', function(event) {
+		var $anchor = $(this)
+		$('html, body').stop().animate({
+			scrollTop: ($($anchor.attr('href')).offset().top - 50)
+		}, 1250, 'easeInOutExpo')
+		event.preventDefault()
+	})
+
 })(jQuery)
